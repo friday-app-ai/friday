dee29e43ebdc83e07d72f180067c1cc3d7a443c7
Sarthak Chaure <sarchaure911@gmail.com>
2024-07-27 16:54:07 +0530

    lesson updated

--- src/components/course/course.tsx
+++ src/components/course/course.tsx
@@ -9,7 +9,7 @@ interface CourseProps {
 
 const Course: React.FC<CourseProps> = ({ id, title, description }) => {
   return (
-    <div className="bg-white shadow-lg rounded-lg overflow-hidden w-[20vw] lg:w-[20vw] h-[35vh] flex flex-col p-4 border border-gray-200">
+    <div className="bg-white shadow-lg rounded-lg overflow-hidden w-[20vw] lg:w-[20vw] h-[40vh] flex flex-col p-4 border border-gray-200">
       <div className="flex flex-col h-full">
         <h2 className="text-2xl font-semibold text-gray-800 mb-2">{title}</h2>
         <div className="text-gray-600 mb-4 flex-grow overflow-y-auto no-scrollbar shadow-sm" style={{ maxHeight: '60%' }}>
